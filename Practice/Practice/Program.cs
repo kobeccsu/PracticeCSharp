aab0028f17c8459e4553d584b107f90dbe2afac2
PRINCETON\Lei_Zhou <Lei_Zhou@epam.com>
2018-12-20 17:31:09 +0800

    init

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var result = from item in arr
+                         where item % 2 == 0
+                         select item;
+
+            // Querable
+        }
+    }
+}
